c90f3e2e8dea771aa0e46338d9aff3f97dc0bff3
SiddhiBhadange <siddhi2464@gmail.com>
2026-02-17 14:19:34 +0530

    first commit

---
+++ src/test/java/com/secondproj/revconnect/RevconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secondproj.revconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevconnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
