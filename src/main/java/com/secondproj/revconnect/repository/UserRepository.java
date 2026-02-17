c6f3e58f50eb8044c3adc1bc175aa18c93b8a0d5
SiddhiBhadange <siddhi2464@gmail.com>
2026-02-17 14:51:02 +0530

    User Repository

---
+++ src/main/java/com/secondproj/revconnect/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.secondproj.revconnect.repository;
+import com.secondproj.revconnect.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmailOrUsername(String email, String username);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
+}
